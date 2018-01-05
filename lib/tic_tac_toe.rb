02b7c4ea06769d6243ffe40ba82dc925414e7314
Allyson Wesman <allysonwesman@gmail.com>
2018-01-05 07:01:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,6 +17,14 @@ def draw?(board)
   full?(board) && !won?(board)
 end
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 def full?(board)
   return board.all? do |space|
     space != " " && space != nil
